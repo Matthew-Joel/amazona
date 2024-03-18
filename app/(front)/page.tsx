49db3673556930fe8c26e43a4845b54a0b6f6c0a
Matthew-Joel <joelmathew826@gmail.com>
2024-03-18 21:53:08 +0100

    product Item

--- app/(front)/page.tsx
+++ app/(front)/page.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
-
+import data from "@/components/header/lib/data";
+import ProductItems from "@/components/products/ProductItems";
 export default function Home() {
   return (
     <>
-    <h2 className="text-2xl py-2">
-      Latest products
-    </h2>
+      <h2 className="text-2xl py-2">Latest products</h2>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4 ">
+   {data.products.map((product) => (
+    <ProductItems key={product.slug} product={product}/>
+   ))}
+      </div>
     </>
-  )
+  );
 }
-    
---
+++ components/header/lib/data.tsx
@@ -0,0 +1,73 @@
+  const data = {
+  products:[
+    {
+        name:'Free Shirt',
+        slug:'free-shirt',
+        category:'Shirts',
+        image:'/images/shirt1.jpg',
+        price:50,
+        brand:'Nike',
+        rating:4.5,
+        numReviews:8,
+        countInStock:20,
+        description:'A popular shirt',
+        isFeatured:true,
+        banner:'/images/banner1.jpg' 
+    },
+    {
+        name:'Fit Shirt',
+        slug:'fit-shirt',
+        category:'Shirts',
+        image:'/images/shirt2.jpg',
+        price:80,
+        brand:'Addida',
+        rating:3.5,
+        numReviews:6,
+        countInStock:20,
+        description:'A popular shirt',
+        isFeatured:true,
+        banner:'/images/banner2.jpg' 
+    },
+    {
+        name:'Slim Shirt',
+        slug:'slim-shirt',
+        category:'Shirts',
+        image:'/images/shirt3.jpg',
+        price:90,
+        brand:'Joelly',
+        rating:4.5,
+        numReviews:18,
+        countInStock:20,
+        description:'A popular shirt',
+        isFeatured:true,
+        banner:'/images/banner1.jpg' 
+    },
+    {
+        name:'Golf Pants',
+        slug:'golf-pants',
+        category:'Pants',
+        image:'/images/pants1.jpg',
+        price:50,
+        brand:'Nike',
+        rating:4.5,
+        numReviews:8,
+        countInStock:20,
+        description:'smart looking pant',
+        isFeatured:true,
+    },
+    {
+        name:'Fit Pants',
+        slug:'fit-pants',
+        category:'Pants',
+        image:'/images/pants2.jpg',
+        price:50,
+        brand:'Zara',
+        rating:4.5,
+        numReviews:8,
+        countInStock:20,
+        description:'smart looking pant',
+        isFeatured:true,
+    },
+  ]  
+}
+export default data
